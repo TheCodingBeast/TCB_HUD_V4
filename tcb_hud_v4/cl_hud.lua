1d6a790d2cfc79f28cab6644e5267cac72def3c7
TheCodingBeast <thecodingbeast@gmail.com>
2015-01-16 21:40:50 +0100

    Update 16-01-2015
    Fixed lockdown error.

--- tcb_hud_v4/cl_hud.lua
+++ tcb_hud_v4/cl_hud.lua
@@ -515,7 +515,7 @@ local function LockDown()
 	if GetGlobalBool("DarkRP_LockDown") then
 
 		local cin = (math.sin(CurTime()) + 1) / 2
-		local chatBoxSize = math.floor(Scrh / 4)
+		local chatBoxSize = math.floor(ScrH() / 4)
 
 		draw.DrawNonParsedText(DarkRP.getPhrase("lockdown_started"), "ScoreboardSubtitle", chbxX, chboxY + chatBoxSize, Color(cin * 255, 0, 255 - (cin * 255), 255), TEXT_ALIGN_LEFT)
 	
